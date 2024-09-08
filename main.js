c3c8125e3d7d77f5e7a304131d0f4749f531bdf6
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-09-07 23:01:15 -0400

    FEAT: Add basic forn validation styling

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // Here goes javascript code
+
+const email = document.querySelector("#email");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
