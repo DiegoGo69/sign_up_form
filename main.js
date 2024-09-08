8e53d77b2056d05487bfaf84e121aa97ba3cb426
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-09-08 16:17:37 -0400

    REFACTOR: Separate password validation handlers
    To avoid repetition define password validation functions on their own
    Define validatePwd and validatePwd2 and call them on the respective
    Event handler, so they are defined once and called both for
    Lazy and Agressive validation event handlers

--- main.js
+++ main.js
@@ -1,85 +1,74 @@
 const log = console.log;
 
+function setInvalid(e) {
+  e.classList.remove('valid');
+  e.classList.add('invalid');
+}
+
+function setValid(e) {
+  e.classList.remove('invalid');
+  e.classList.add('valid');
+}
 // Password lazy validation
 const pwd = document.querySelector("#pwd");
 const pPwd = document.querySelector("#pwd+ p")
-pwd.addEventListener('change', event => {
-  // if too short
-  if (pwd.validity.tooShort) {
-    pPwd.textContent = 'Password too short';
-    pwd.setCustomValidity("Password confirm too short");
-    pwd.classList.add('invalid');
-  }
-  // else if valid
-  else {
-    pPwd.textContent = "Password valid :)";
-    pwd.setCustomValidity("");
-    pwd.classList.remove('invalid');
-  } 
-})
+pwd.addEventListener('change', () => validatePwd(pwd, pPwd));
 
-// Confirm password lazy validation
+// Confirmation password lazy validation
 const pwd2 = document.querySelector("#pwd_confirm");
 const pPwd2 = document.querySelector("#pwd_confirm + p")
-pwd2.addEventListener('change', event => {
-  // if too short
-  if (pwd2.validity.tooShort) {
-    pPwd2.textContent = 'Password too short';
-    pwd2.setCustomValidity("Password confirm too short");
-    pwd2.classList.add('invalid');
-  }
-  // if long enough but does not match
-  else if (pwd2.value !== pwd.value) {
-    pPwd2.textContent = "Passwords do not match";
-    pwd2.setCustomValidity("Passwords do not match");
-    pwd2.classList.add('invalid');
-  }
-  // else if valid
-  else {
-    pPwd2.textContent = "Passwords match";
-    pwd2.setCustomValidity("");
-    pwd2.classList.remove('invalid');
-  } 
-})
+pwd2.addEventListener('change', () => validatePwd2(pwd2, pPwd2));
 
 // Password agressive validation when already invalid
-pwd.addEventListener('input', event => {
-  if (pwd.classList.contains('invalid')) {
-  // if too short
-  if (pwd.validity.tooShort) {
-    pPwd.textContent = 'Password too short';
-    pwd.setCustomValidity("Password confirm too short");
+pwd.addEventListener('input', () => {
+  if (pwd.classList.contains('invalid') || pwd.classList.contains('valid')) {
+    validatePwd(pwd, pPwd);
   }
-  // else if valid
-  else {
-    pPwd.textContent = "Password valid :)";
-    pwd.setCustomValidity("");
-    pwd.classList.remove('invalid')
-  }};
-})
+});
+// Confirmation Password agressive validation when already invalid
+pwd2.addEventListener('input', () => {
+  if (pwd2.classList.contains('invalid') || pwd2.classList.contains('valid')) {
+    validatePwd2(pwd2, pPwd2);
+  }
+});
 
-// Confirm Password agressive validation when already invalid
-pwd2.addEventListener('input', event => {
-  if (pwd2.classList.contains('invalid')) {
-        // if too short
-    if (pwd2.validity.tooShort) {
-      pPwd2.textContent = 'Password too short';
-      pwd2.setCustomValidity("Password confirm too short");
-      pwd2.classList.add('invalid');
-    }
-    // if long enough but does not match
-    else if (pwd2.value !== pwd.value) {
-      pPwd2.textContent = "Passwords do not match";
-      pwd2.setCustomValidity("Passwords do not match");
-      pwd2.classList.add('invalid');
+// Password agressive validation
+function validatePwd(p, pP) {
+    // if too short
+    if (p.validity.tooShort) {
+      pP.textContent = 'Password too short';
+      p.setCustomValidity("Password confirm too short");
+      setInvalid(p);
     }
     // else if valid
     else {
-      pPwd2.textContent = "Passwords match";
-      pwd2.setCustomValidity("");
-      pwd2.classList.remove('invalid');
-    }};
-})
+      pP.textContent = "Password valid :)";
+      p.setCustomValidity("");
+      setValid(p);
+    }
+};
+
+// Confirm password agressive validation
+function validatePwd2(p, pP) {
+     // if too short
+    if (p.validity.tooShort) {
+      pP.textContent = 'Password too short';
+      p.setCustomValidity("Password confirm too short");
+      setInvalid(p);
+    }
+    // if long enough but does not match
+    else if (p.value !== pwd.value) {
+      pP.textContent = "Passwords do not match";
+      p.setCustomValidity("Passwords do not match");
+      setInvalid(p);
+    }
+    // else if valid
+    else {
+      pP.textContent = "Passwords match";
+      p.setCustomValidity("");
+      setValid(p);
+    }
+}
 
 
 // some testing
