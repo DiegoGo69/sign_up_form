83666a69e6340c48f6d6aa196b17ba641161012d
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-09-08 12:11:01 -0400

    FIX: P inside P testing

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Here goes javascript code
 
-const email = document.querySelector("#email");
+// /* TESTING P INSIDE P SELECTION (None works)*/
+const pName = document.querySelector("p > p");
+pName.textContent = "VALID";
 
+const pName = document.querySelector("input + p");
+pName.textContent = "VALID";
+
+
+// some testing
+const email = document.querySelector("#email");
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
     email.setCustomValidity("I am expecting an email address!");
